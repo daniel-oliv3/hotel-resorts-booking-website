737b0b237899bbd77770dec3a8f7c670d7ffaabe
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-23 11:17:33 -0300

    Menu navbar toggle

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* ======= Header Navbar, Menu Toggle ======= */
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
